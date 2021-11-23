9c4bb7613176c8b0a4fa61ef79943a86548191cb
seoeunhyo <93567740+seoeunhyo@users.noreply.github.com>
2021-11-23 23:29:11 +0900

    Add files via upload

--- app/src/main/java/com/example/appstudy/MainActivity.java
+++ app/src/main/java/com/example/appstudy/MainActivity.java
@@ -1,34 +1,39 @@
-package com.example.finalproject;
+package com.example.appstudy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sec);
+        setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.btn2);
-        btn.setOnClickListener(new View.OnClickListener(){
-
-
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SecActivity.class);
-                startActivity(intent);
+
             }
         });
+
+
+
+
     }
 
-    public void back(View view) {
-//선택완료창으로
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
     }
-
-
 }

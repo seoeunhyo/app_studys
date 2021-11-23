ad97753eed1a78d79e32df15349f23a4a9e8860e
김재훈 <93564387+jae-hoonkim@users.noreply.github.com>
2021-11-23 23:19:54 +0900

    Add files via upload

--- app/src/main/java/com/example/appstudy/MainActivity.java
+++ app/src/main/java/com/example/appstudy/MainActivity.java
@@ -1,46 +1,34 @@
-package com.example.appstudy;
+package com.example.finalproject;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);{
-            if(requestCode == 100){
-                if(resultCode == RESULT_OK){
-                    Toast.makeText(MainActivity.this, "result ok!", Toast.LENGTH_SHORT).show();
-
-
-                }
-            }
-        };
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sec);
 
-        Button btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn = (Button)findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                intent.putExtra("name","gildong");
-                startActivityForResult(intent,100);
+                Intent intent = new Intent(getApplicationContext(), SecActivity.class);
+                startActivity(intent);
             }
         });
-
-
-
-
     }
+
+    public void back(View view) {
+//선택완료창으로
+    }
+
+
 }
